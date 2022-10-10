5b8988bf32b62e33b8e832d483141a835b79ddf3
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2022-10-10 23:51:11 +0200

    pr

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '52ecec9078b2aac126d797c2794dd70a8fe7c7c3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '52ecec9078b2aac126d797c2794dd70a8fe7c7c3',
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap-icons' => array(
+            'pretty_version' => 'v1.9.1',
+            'version' => '1.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap-icons',
+            'aliases' => array(),
+            'reference' => 'f7f6f61194419dcf4447dbe05b78a72601646490',
+            'dev_requirement' => false,
+        ),
+    ),
+);
